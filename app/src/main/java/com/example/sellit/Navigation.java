db5ec1ac529afb5f0d51995707b81555b34815d0
cyber-psych0 <ayushman.verma159@gmail.com>
2019-08-01 11:51:52 +0530

    Added data upload feature

--- app/src/main/java/com/example/sellit/Navigation.java
+++ app/src/main/java/com/example/sellit/Navigation.java
@@ -36,8 +36,11 @@ public class Navigation extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+
+
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                       // .setAction("Action", null).show();
             }
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
---
+++ app/src/main/java/com/example/sellit/Upload.java
@@ -0,0 +1,4 @@
+package com.example.sellit;
+
+public class Upload {
+}
