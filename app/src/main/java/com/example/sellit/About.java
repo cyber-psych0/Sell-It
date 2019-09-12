e67af544dd3a50518ce77c18d0e0bd4d44ebdd66
rohitverma1999hack <rohitvermaavcbbk2011@gmail.com>
2019-09-12 18:54:37 +0530

    Add Functionality to about activity

--- app/src/main/java/com/example/sellit/About.java
+++ app/src/main/java/com/example/sellit/About.java
@@ -2,13 +2,63 @@ package com.example.sellit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
-public class About extends AppCompatActivity {
+public class About extends AppCompatActivity implements View.OnClickListener {
+
+    private LinearLayout star;
+    private LinearLayout Rohit;
+    private LinearLayout ayshman;
+    private LinearLayout nikhil;
+    private LinearLayout akhil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        star = findViewById(R.id.Star_on_github);
+        Rohit = findViewById(R.id.Rohit_about);
+        ayshman = findViewById(R.id.Ayusman_about);
+        nikhil = findViewById(R.id.nikhil);
+        akhil = findViewById(R.id.akhil);
+        ayshman.setOnClickListener(this);
+        nikhil.setOnClickListener(this);
+        Rohit.setOnClickListener(this);
+        akhil.setOnClickListener(this);
+        star.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id){
+            case R.id.Star_on_github:
+                openweb("https://github.com/cyber-psych0/Sell-It");
+                break;
+            case R.id.Rohit_about:
+                openweb("https://github.com/refactor-droidyy");
+                break;
+            case R.id.nikhil:
+                openweb("hhttps://github.com/Debug-The-Code");
+                break;
+            case R.id.akhil:
+                openweb("https://github.com/D1ABL3");
+                break;
+            case R.id.Ayusman_about:
+                openweb("https://github.com/cyber-psych0/");
+            default :
+                break;
+        }
+
+    }
+    public void openweb(String url) {
+
+        Intent implicit = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        startActivity(implicit);
     }
 }
